efdadb23e5d8b730410f3c6a3836ee398433e710
MinJi Kwon <minjik95@naver.com>
2017-05-27 08:55:42 +0000

    ...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  def home
+  
+  end
+  
   def index
-     @posts=Post.all
+    @posts=Post.all
   end
   
   def new
@@ -8,36 +12,31 @@ class PostsController < ApplicationController
   end
   
   def create
-     @post=Post.new
-     @post.name=params[:name]
-     @post.address=params[:address]
-     @post.save
+    @post=Post.new
+    @post.name=params[:name]
+    @post.address=params[:address]
+    @post.save
      
-     redirect_to '/index'
+     redirect_to "/index"
   end
   
   def destroy
-      @destroypost=Post.find(params[:post_id])
-      @destroypost.destroy
+    @destroypost=Post.find(params[:post_id])
+    @destroypost.destroy
       
       redirect_to '/'
   end
   
- def edit
-        @editpost=Post.find(params[:post_id])
-       
-    end
+  def edit
+      @editpost=Post.find(params[:post_id])
+  end
     
-    def update
-
-        @updatepost=Post.find(params[:post_id])
-        @updatepost.name=params[:name]
-        @updatepost.address=params[:address]
-        @updatepost.save
+  def update
+      @updatepost=Post.find(params[:post_id])
+      @updatepost.name=params[:name]
+      @updatepost.address=params[:address]
+      @updatepost.save
         
-        redirect_to '/'
-        
-    end
-  
-  
+      redirect_to '/'
+  end
 end
